5462f8f5f24092b27e583429107967564644d62d
root <root@orangepipcplus>
2021-08-14 16:58:36 -0300

    add comando abrir portão

--- stream_video.py
+++ stream_video.py
@@ -2,11 +2,18 @@
 """
 Created on Thu Nov 14 18:57:44 2019
 
-@author: seraj
+@author: Arnald A. Martins
 """
-import time
+from time import sleep
 import cv2 
 from flask import Flask, render_template, Response
+import OPi.GPIO as GPIO
+
+pinoRele = 37
+
+GPIO.setboard(GPIO.PCPCPLUS)    # Orange Pi PC board
+GPIO.setmode(GPIO.BOARD)        # set up BOARD BCM numbering
+GPIO.setup(pinoRele, GPIO.OUT)         # set BCM7 (pin 26) as an output (LED)
 
 app = Flask(__name__)
 
@@ -22,16 +29,25 @@ def video_feed():
     return Response(gen(),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
+
 @app.route('/open_door', methods=['POST'])
 def open_door():
-    """Enviar comando para abrir o portão"""
-    print("aqui o camando de abrir portão")
-    return ("nothing")
+    pinoRele = 37
+    try:
+    	GPIO.output(pinoRele, 1)       # set port/pin value to 1/HIGH/True
+    	sleep(0.3)
+    	GPIO.output(pinoRele, 0)       # set port/pin value to 0/LOW/False
+    	sleep(0.3)
+#        GPIO.cleanup()
+    	return ("nothing")
+    except:
+    	GPIO.output(pinoRele, 0)
+
 
 def gen():
     """Video streaming generator function."""
     # mudar para 1 quando no OrangePi
-    cap = cv2.VideoCapture(0) 
+    cap = cv2.VideoCapture(1) 
 
     # Read until video is completed
     while(cap.isOpened()):
@@ -41,11 +57,7 @@ def gen():
             img = cv2.resize(img, (0,0), fx=1, fy=1) 
             frame = cv2.imencode('.jpg', img)[1].tobytes()
             yield (b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
-            time.sleep(0.1)
+            sleep(0.1)
         else: 
             break
-       
-
-
-    
 
